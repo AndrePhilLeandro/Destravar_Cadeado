ab49322b106657c0b8ae7c1d787e75e085f64898
Andre Leandro <andrephilleandro@gmail.com>
2024-10-17 17:11:31 -0300

    Criaçao do repositorio, e adicionado arquivos.

---
+++ Destranca_Cadeado/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Destranca_Cadeado
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int op;
+            do
+            {
+                Console.Write("" +
+                    "1 - Iniciar\n2 - Regras \nDigite a opçao desejada:");
+                     op = int.Parse(Console.ReadLine());
+                if (op >=3)
+                {
+                    Console.WriteLine("Digito incorreto, selecione 1 ou 2. \n");
+                }
+                switch (op)
+                {
+                    case 1:
+                        Console.WriteLine("Iniciando Jogo");
+                    break;
+
+                    case 2:
+                        Console.WriteLine("Regras do Jogo \n");
+                        Console.WriteLine("Ao iniciar o jogo selecione quantos digitos vai ter, para que a senha seja descoberta,\na quantidade de tentativas será igual a quantidade de digitos. \n");
+                        Console.WriteLine("Jogador ganha quando descobrir todos os digitos para destravar o cadeado antes do acabar todas as tentativas. \n");
+                    break;                
+                }
+            }
+            while (op != 1);
+            int d, tentativa;
+            Console.Write("Digite quantos digitos terá o cadeado: ");
+            d=int.Parse(Console.ReadLine());
+            tentativa = d*2;
+
+            int[] cadeado = new int[d];
+            int[] Descobrir = new int[d];
+            Random rand = new Random();
+
+            for(int i = 0; i < cadeado.Length; i++)
+            {
+                cadeado[i] = rand.Next(0,9);
+            }
+            Console.WriteLine("Começando o jogo");
+            bool jogoVencido = false;
+            while (tentativa !=0 && !jogoVencido)
+            {
+                int digitoCerto = 0;
+                for (int i = 0; i < Descobrir.Length; i++)
+                {
+                    int digito = 0;
+                    Console.Write($"Digite o {i + 1}° numero: ");
+                    digito = int.Parse(Console.ReadLine());
+                    Descobrir[i] = digito ;
+                    if (digito == cadeado[i])
+                    {
+                        Console.WriteLine("O numero esta correto.");
+                        digitoCerto++;
+                        tentativa--;
+                        Console.WriteLine($" De {d * 2} tentativas so restam: {tentativa}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Numero incorreto");
+                        tentativa--;
+                        Console.WriteLine($" De {d * 2} tentativas so restam: {tentativa}");
+                        if (tentativa == 0)
+                        {
+                            Console.WriteLine($"Voce perdeu, Suas tentativas chegaram a 0");
+                            Console.WriteLine("A senha do cadeado era: ");
+                            for (int j = 0; j < cadeado.Length; j++)
+                            {
+                                Console.Write(" " +cadeado[j] +"\n");
+                            }
+                            Console.WriteLine("Sua combinaçao de senha foi: ");
+                            for (int j = 0; j < cadeado.Length; j++)
+                            {
+                                Console.Write(" " + Descobrir[j] +"\n" );
+                            }
+                           break;
+                        }
+                    } 
+                }
+                if(digitoCerto == d)
+                {
+                    Console.WriteLine("A senha do cadeado era: ");
+                    for (int j = 0; j < cadeado.Length; j++)
+                    {
+                        Console.Write(" " + cadeado[j] +"\n");
+                    }
+                    Console.WriteLine("Sua combinaçao de senha foi: ");
+                    for (int j = 0; j < cadeado.Length; j++)
+                    {
+                        Console.Write(" " +Descobrir[j] + "\n");
+                    }
+                    Console.WriteLine("Parabéns! Você destravou o cadeado!");
+                    jogoVencido |= true;
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,30 @@
+
 # Destravar_Cadeado
+
+ <h1 >  Destranca Cadeado  </h1>
+ <h3> Descrição </h3>
+ Destranca cadeado e um jogo simples desenvolvido em C# com intuito de praticar e aprimorar habilidades e logica de programação.
+
+ O objetivo do jogo e tentar descobrir a combinação correta para destrancar um cadeado virtual antes que todas as tentativas se esgotem.
+
+ <h3> Como Jogar </h3>
+ 
+ 1 - Ao iniciar o jogo, o jogador escolhe quantos dígitos terá o cadeado.
+ 2 - O número de tentativas será o dobro da quantidade de dígitos escolhidos.
+ 3 - O jogador deve adivinhar os números que forma a senha o cadeado.
+ 4  - A cada tentativa, o jogador será informado se os números digitados estão corretos ou não.
+ 5 - O jogo termina quando:
+     - O jogador advinha a combinação da senha,
+     ou
+     - A tentativa acaba e o jogador perde o jogo.
+
+<h3> Tecnologia Utilizada </h3>
+- Linguagem: C#
+- Framework.NET
+
+<h3> Como executar </h3>
+- clone o repositório
+- Execute o arquivo "Destranca_Cadeado.exe"
+
+<h3> Autor </h3>
+- Desenvolvido por André Leandro
